f701e196c1e3fe3d142268c1a837e73e6bf2b1ea
ecogreenpro <ecogreenproglobal1@gmail.com>
2024-05-05 17:42:45 +0600

    app.css remove

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import "./App.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
-// App.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home';
 import SetAvatar from './Pages/Avatar/setAvatar';
-
-
 
 const App = () => {
   return (
